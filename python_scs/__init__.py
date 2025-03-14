15c06cf53c2b08c4c9234588af63d30f04586ae1
Iago <iago.almeida@visie.com.br>
2025-03-14 20:39:53 -0300

    ✨ Atualizando README.md

--- python_scs/__init__.py
+++ python_scs/__init__.py
@@ -2,4 +2,4 @@ from .core import PythonScriptsCronManager
 from .schemas import PythonCronItem
 
 __all__ = ["PythonScriptsCronManager", "PythonCronItem"]
-__version__ = "0.1.0"
+__version__ = "1.0"
